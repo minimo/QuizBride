acf4f3d9df0e54245f040c586e33668c168cb71d
minimo <minimo.mobile@gmail.com>
2015-01-21 18:00:37 +0900

    仮の絵を入れた

--- src/assets.js
+++ src/assets.js
@@ -15,6 +15,33 @@ quiz.assets = {
     //Font
     "yasasihsa":  "font/07YasashisaAntique.ttf",
     "scoreboard": "font/scoreboard.ttf",
+
+    //Slide
+    "Q01-0":"slide/Q01-0.jpg",
+    "Q01-1":"slide/Q01-1.jpg",
+    "Q01-2":"slide/Q01-2.jpg",
+    "Q01-3":"slide/Q01-3.jpg",
+    "Q01-4":"slide/Q01-4.jpg",
+
+    "Q02-0":"slide/Q02-0.jpg",
+    "Q02-1":"slide/Q02-1.jpg",
+    "Q02-2":"slide/Q02-2.jpg",
+    "Q02-3":"slide/Q02-3.jpg",
+
+    "Q03-0":"slide/Q03-0.jpg",
+    "Q03-1":"slide/Q03-1.jpg",
+    "Q03-2":"slide/Q03-2.jpg",
+    "Q03-3":"slide/Q03-3.jpg",
+
+    "Q04-0":"slide/Q04-0.jpg",
+    "Q04-1":"slide/Q04-1.jpg",
+    "Q04-2":"slide/Q04-2.jpg",
+    "Q04-3":"slide/Q04-3.jpg",
+
+    "Q05-0":"slide/Q05-0.jpg",
+    "Q05-1":"slide/Q05-1.jpg",
+    "Q05-2":"slide/Q05-2.jpg",
+    "Q05-3":"slide/Q05-3.jpg",
 }
 
 })();
--- src/slide/Slide1.js
+++ src/slide/Slide1.js
@@ -6,28 +6,31 @@
  */
 (function() {
 
-var SLIDE_DEFAULT_PARAM = {
-    title: "テスト問題",
-    question1: "「夜は短し歩けよ乙女」「有頂天家族」などの著作で知られる森見登美彦ですが",
-    question2: "彼のデビュー作であり、山本周五郎賞を受賞した作品は",
-    question3: "以下のうち、どれでしょうか",
-
-    answer1:"「四畳半神話体系」",
-    answer2:"「ペンギンハイウェイ」",
-    answer3:"「太陽の塔」",
-    answer4:"「芸術は爆発だ！」",
-
-    answer: 3,
-};
-
 tm.define("quiz.Slide1", {
     superClass: "quiz.SlideBase",
 
     labelParam: {fontFamily: "Yasashisa", align: "center", baseline: "middle",outlineWidth: 2, fontWeight:700},
 
+    question: {
+        title: "テスト問題",
+        question1: "「夜は短し歩けよ乙女」「有頂天家族」などの著作で知られる森見登美彦ですが",
+        question2: "彼のデビュー作であり、山本周五郎賞を受賞した作品は",
+        question3: "以下のうち、どれでしょうか",
+
+        answer1: "「四畳半神話体系」",
+        answer2: "「太陽の塔」",
+        answer3: "「芸術は爆発だ！」",
+
+        answer: 2,
+};
+
     init: function(param) {
         this.superInit();
         this.time = 0;
+
+        this.slide0 = tm.display.Sprite("Q1-0")
+            .addChildTo(this)
+            .setPosition(SC_W*0.5, SC_H*0.5);
 
         this.think = quiz.ThinkingTime(5)
             .setPosition(0, 0);
