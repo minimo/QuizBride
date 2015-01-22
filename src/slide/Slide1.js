1c3444da80a235a611afa9070faa4ff10c09278d
minimo <minimo.mobile@gmail.com>
2015-01-22 19:26:55 +0900

    更新

--- src/slide/Slide1.js
+++ src/slide/Slide1.js
@@ -12,25 +12,37 @@ tm.define("quiz.Slide1", {
     labelParam: {fontFamily: "Yasashisa", align: "center", baseline: "middle",outlineWidth: 2, fontWeight:700},
 
     question: {
-        title: "テスト問題",
-        question1: "「夜は短し歩けよ乙女」「有頂天家族」などの著作で知られる森見登美彦ですが",
-        question2: "彼のデビュー作であり、山本周五郎賞を受賞した作品は",
-        question3: "以下のうち、どれでしょうか",
+        "1": "何事にも万全を持って臨む新郎",
+        "2": "それは初デートの時も例外ではありません",
+        "3": "さて、この「３」という数字",
+        "4": "これは初デートに関係ある数字だそうです",
+        "5": "それは以下のうちどれでしょうか？",
 
-        answer1: "「四畳半神話体系」",
-        answer2: "「太陽の塔」",
-        answer3: "「芸術は爆発だ！」",
+        "question1":"Ｑ１．初デートにまつわる数字",
+        "question2":"「３」",
+        "question3":"これは何の数字でしょうか？",
 
-        answer: 2,
+        "answer1": "提案したプランの数",
+        "answer2": "残りの変身回数",
+        "answer3": "渡したプレゼントの数",
+
+        "answer": 1,
     },
+
+    //現在スライドシーケンス番号
+    seq: 0,
+    phase: 0,
 
     init: function(param) {
         this.superInit();
         this.time = 0;
 
-        this.slide0 = tm.display.Sprite("Q01-0")
-            .addChildTo(this)
-            .setPosition(SC_W*0.5, SC_H*0.5);
+        this.slide = [];
+        for (var i = 0; i < 5; i++) {
+            this.slide[i] = tm.display.Sprite("Q01-"+i)
+                .addChildTo(this)
+                .setPosition(SC_W*2, SC_H*0.5);
+        }
 
         this.think = quiz.ThinkingTime(60)
             .setPosition(0, 0);
@@ -38,9 +50,32 @@ tm.define("quiz.Slide1", {
 
     update: function() {
         var kb = app.keyboard;
-        if (kb.getKeyDown("s")) {
-            this.think.addChildTo(this);
+        if (kb.getKeyDown("s") && this.time > 15) {
+            this.phase++;
+            this.time = 0;
         }
+        if (this.phase == 1) {
+            this.slide[this.seq].tweener.clear().moveBy(-SC_W*1.5, 0, 500, "easeOutSine");
+            this.seq++;
+            this.phase++;
+        }
+        if (this.phase == 3) {
+            this.slide[this.seq].tweener.clear().moveBy(-SC_W*1.5, 0, 500, "easeOutSine");
+            this.seq++;
+            this.phase++;
+        }
+        if (this.phase == 5) {
+            this.enterMessage(SC_H*0.8, 3000, this.question[1]);
+            this.phase++;
+        }
+        if (this.phase == 7) {
+//            this.slide[this.seq].tweener.clear().moveBy(-SC_W*1.5, 0, 500, "easeOutSine");
+            this.enterMessage(SC_H*0.8, 3000, this.question[2]);
+//            this.seq++;
+            this.phase++;
+        }
+
+        this.time++;
     },
 });
 
--- src/slide/SlideBase.js
+++ src/slide/SlideBase.js
@@ -9,12 +9,61 @@
 tm.define("quiz.SlideBase", {
     superClass: "tm.app.Scene",
 
+    question: {
+        "1": "何事にも万全を持って臨む新郎",
+        "2": "それは初デートの時も例外ではありません",
+        "3": "さて、この「３」という数字",
+        "4": "これは初デートに関係ある数字だそうです",
+        "5": "それは以下のうちどれでしょうか？",
+
+        "question1":"Ｑ１．初デートにまつわる数字",
+        "question2":"「３」",
+        "question3":"これは何の数字でしょうか？",
+
+        "answer1": "提案したプランの数",
+        "answer2": "残りの変身回数",
+        "answer3": "渡したプレゼントの数",
+
+        "answer": 1,
+    },
+
+    msg: null,
+
     init: function() {
         this.superInit();
         this.time = 0;
     },
 
     update: function() {
+    },
+
+    //メッセージ表示
+    enterMessage: function(y, time, msg, size) {
+        size = size || 45;
+        var that = this;
+
+        if (this.msg) {
+            this.msg.remove();
+            this.msg = null;
+        }
+
+        this.msg = tm.app.Object2D().addChildTo(this);
+//        pt.tweener.wait(time).call(function(){this.remove();that.msg=null;}.bind(pt));
+
+        tm.display.RectangleShape(SC_W*0.8, 80, {fillStyle: "rgba(0,0,0,0.5)", strokeStyle: "rgba(0,0,0,0.5)"})
+            .addChildTo(this.msg)
+            .setPosition(SC_W*0.5, y);
+
+        tm.display.OutlineLabel(msg, size)
+            .addChildTo(this.msg)
+            .setParam(this.labelParam)
+            .setPosition(SC_W*0.5, y);
+    },
+    eraseMessage: function() {
+        if (this.msg) {
+            this.msg.remove();
+            this.msg = null;
+        }
     },
 });
 
